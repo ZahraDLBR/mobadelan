e97d3f3c4996793a3269e139de6f23cf187fd690
saharzargarzadeh <sahar.zargarzadeh76@gmail.com>
2018-08-12 17:57:13 +0430

    some nonimportant changes :)) but in origin master

--- manager/views.py
+++ manager/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 #sallllaaaaaam
-#brannnnch
+
 def index(request):
 
     return "manger"
--- staff/views.py
+++ staff/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def index(request):
-    return "staff"
+def workerpanel(request):
+    context = {
+        'name': "workerpanel",
+    }
+    return render(request, 'templates/staff/workerpanel.html', context)
+
+def detail(request):
+    return None
